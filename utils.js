5d2fcb95635431a94dd667e67e945dd5070b0661
matteoeeeed <95379963+matteoeeeed@users.noreply.github.com>
2023-02-05 10:54:01 +0100

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,15 @@
+function Saluta(nome){
+console.log("Hi "+nome)
+}
+function Saluta2(nome){
+console.log("Hi "+nome)
+}
+function Saluta3(nome){
+console.log("Hi "+nome)
+}
+function Saluta4(nome){
+console.log("Hi "+nome)
+}
+
+
+module.exports = {saluta: Saluta,saluta2:Saluta2,saluta3:Saluta3,saluta4:Saluta4}
