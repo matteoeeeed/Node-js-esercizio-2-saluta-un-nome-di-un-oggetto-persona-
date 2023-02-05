3a80e1f1aaff51f79f7c5f6a798196b1b246a18b
matteoeeeed <95379963+matteoeeeed@users.noreply.github.com>
2023-02-05 10:49:47 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const a=10
+let i=0;
+const utils=require('./utils')
+//importate nell'index persona
+
+utils.saluta('marco')
+utils.saluta2('matteo')
+utils.saluta3('luca')
+utils.saluta4('giovanni')
+//utilizzare persona ...(attenti)
+// voglio salutare un solo nome dei nomi importati (dal modulo persona)
